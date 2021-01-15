d6b471d704a1a3ceb392c3eebf400679b0571604
Daniel Strizhevsky <danielstrizhevsky@gmail.com>
2021-01-14 19:18:32 -0800

    Add documentation for rrt_base.py

--- rrt_base.py
+++ rrt_base.py
@@ -1,7 +1,9 @@
+from abc import ABC, abstractmethod
 from collections import defaultdict
 from matplotlib import pyplot as plt
 from matplotlib.patches import Rectangle
 from math import log2
+from typing import List, Tuple
 import random
 import time
 
@@ -16,7 +18,7 @@ from rrt_config import (
 )
 
 
-class RRTBase:
+class RRTBase(ABC):
     def __init__(self, start, goal):
         self.points = [start]
         self.parents = {}
@@ -32,7 +34,10 @@ class RRTBase:
         self.agent_viz = None
         self.goal_viz = None
 
-    def init_plot(self):
+    def init_plot(self) -> None:
+        """
+        Set bounds and draw obstacles, goal, and agent.
+        """
         plt.xlim([0, RIGHT_BOUND])
         plt.ylim([0, BOTTOM_BOUND])
         plt.gca().set_aspect("equal")
@@ -48,16 +53,36 @@ class RRTBase:
         )
         self.agent_viz = plt.plot(10, 10, "ro", zorder=4)[0]
 
+    @abstractmethod
     def step(self):
+        """
+        Perform one full step of adding a point to the RRT.
+        Includes choosing a random point, steering towards it,
+        rewiring if applicable, etc.
+        """
         pass
 
-    def _is_in_goal(self, point):
+    def _is_in_goal(self, point: Tuple[float, float]) -> bool:
+        """
+        Return whether the given point is within the boundaries
+        of the goal.
+
+        point: (x, y) tuple representing the point
+        return: whether the point is in the goal
+        """
         return (
             self.goal[0][0] <= point[0] <= self.goal[1][0]
             and self.goal[0][1] <= point[1] <= self.goal[1][1]
         )
 
-    def _get_cost(self, point):
+    def _get_cost(self, point: Tuple[float, float]) -> float:
+        """
+        Return the cost from the root of a point in the tree.
+        Cost is the sum of edge lengths from the root to this point.
+
+        point: (x, y) tuple representing the point
+        return: cost from the root to this point along the tree
+        """
         cost = 0
         while point in self.parents:
             parent = self.parents[point]
@@ -65,9 +90,15 @@ class RRTBase:
             point = parent
         return cost
 
-    def _obstacle_free(self, point):
+    def _obstacle_free(self, point: Tuple[float, float]) -> float:
         """
-        return False if point collides with an obstacle
+        Return whether this point is free of obstacles
+
+        point: (x, y) tuple representing the point
+        return: False if the point collides with an obstacle, otherwise True
+
+        TODO: more collision checks along an edge as opposed to just at one
+        point
         """
         for obstacle in OBSTACLES:
             left, top = obstacle[0]
@@ -76,7 +107,13 @@ class RRTBase:
                 return False
         return True
 
-    def best_path_to_goal(self):
+    def best_path_to_goal(self) -> List[Tuple[float, float]]:
+        """
+        Return the lowest-cost path to the goal from the root of the RRT.
+
+        return: the shortest path, comprised of a list of points
+        in order from root to goal.
+        """
         points_in_goal = [point for point in self.points if self._is_in_goal(point)]
         if not points_in_goal:
             return []
@@ -89,7 +126,21 @@ class RRTBase:
 
         return path[::-1]
 
-    def take_step_along_path(self, path, to_travel=AGENT_SPEED):
+    def take_step_along_path(
+        self, path: List[Tuple[float, float]], to_travel: float = AGENT_SPEED
+    ) -> None:
+        """
+        Note: path is expected to be in reverse order for computational
+        efficiency.
+        TODO: use a queue to make this more straightforward
+
+        Move the agent "to_travel" units along the path, updating the
+        path to remove points as they're reached.
+
+        path: list of point tuples in reverse order
+        to_travel: distance left to travel, used for recursive calls if
+        the next point is less than to_travel away from the agent
+        """
         if not path:
             return
         point = path[-1]
@@ -107,7 +158,20 @@ class RRTBase:
             )
             self.agent_pos = new_point
 
-    def move_along_path_until_done(self, path, keep_stepping=False, visualize=True):
+    def move_along_path_until_done(
+        self,
+        path: List[Tuple[float, float]],
+        keep_stepping: bool = False,
+        visualize: bool = True,
+    ) -> None:
+        """
+        Keep moving the agent along the path until it's reached the end.
+
+        path: list of point tuples in reverse order
+        keep_stepping: Whether to call the RRT's step function while moving.
+        Useful for anytime planning.
+        visualize: Whether to call plt.pause()
+        """
         while path:
             self.take_step_along_path(path)
             self.agent_viz.set_xdata(self.agent_pos[0])
@@ -117,15 +181,37 @@ class RRTBase:
             if visualize:
                 plt.pause(VIS_PAUSE_LENGTH)
 
-    def _calculate_distance(self, a, b):
+    def _calculate_distance(
+        self, a: Tuple[float, float], b: Tuple[float, float]
+    ) -> float:
+        """
+        Calculate the Euclidean distance between two points
+
+        a: first (x, y) point
+        b: second (x, y) point
+        return: Euclidean distance between a and b
+        """
         return ((b[1] - a[1]) ** 2 + (b[0] - a[0]) ** 2) ** 0.5
 
-    def nearest_neighbor(self, new_point):
+    def nearest_neighbor(self, new_point: Tuple[float, float]) -> Tuple[float, float]:
+        """
+        Return the closest point in the RRT to the given new point
+
+        new_point: (x, y) tuple representing a new point
+        return: (x, y) tuple of a point in the tree nearest to this point.
+        """
         return min(self.points, key=lambda x: self._calculate_distance(new_point, x))
 
-    def get_new_point(self, source, destination):
+    def get_new_point(
+        self, source: Tuple[float, float], destination: Tuple[float, float]
+    ) -> Tuple[float, float]:
         """
-        Returns the new point distance DELTA from the source to the destination
+        Returns a new point distance DELTA from the source, towards the destination
+
+        source: first (x, y) point
+        destination: second (x, y) point to steer from source to
+
+        return: a new (x, y) point distance DELTA closer to destination, from source
         """
         distance = self._calculate_distance(source, destination)
         if distance <= DELTA:
@@ -142,7 +228,11 @@ class RRTBase:
             return None
         return new_point
 
-    def redraw_path_to_goal(self):
+    def redraw_path_to_goal(self) -> None:
+        """
+        Remove the current path to goal from the visualization, and redraw
+        a new one based on the current best path to the goal.
+        """
         for line in self.lines_to_goal:
             line.remove()
         self.lines_to_goal = []
